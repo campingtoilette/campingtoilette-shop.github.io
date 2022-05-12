088430a6f7b3624871293725dce678fd57b2f9c7
campingtoilette <campingtoilette@protonmail.com>
2022-05-12 16:33:23 +0200

    seotextIndex component

---
+++ components/SeoTextIndex.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <!-- About Start -->
+    <div class="container-xxl py-5">
+      <div class="container">
+        <div class="row g-5">
+          <div class="col-lg-12 fadeInUp" style="min-height: 400px">
+            <div class="section-title bg-white text-start text-primary pe-3">
+              Campingtoiletten
+            </div>
+            <h1 class="mb-4">Was ist eine Campingtoilette?</h1>
+
+            <p class="mb-4">
+              Grundsätzlich handelt es sich bei einer Campingtoilette um eine
+              Toilettenlösung, die für die Nutzung beim Camping geeignet ist.
+              Bei der Wahl der passenden Campingtoilette hat man als Camper
+              diverse Optionen. Wir von campingtoilette.shop haben es uns zur
+              Aufgabe gemacht, die besten Campingtoiletten für unsere Leser
+              aufzuspüren und miteinander zu vergleichen.
+            </p>
+
+            <p class="mb-4">
+              Wir konzentrieren uns bei unserer Suche überwiegend auf
+              Trenntoiletten, da diese unserer Ansicht nach optimal für die
+              Verwendung beim Camping sind – und genau darum geht es bei uns,
+              welche Campingtoilette ist die beste?
+            </p>
+
+            <h5 class="mb-3 mt-5">Die Vorteile von Campingtoiletten im Überblick</h5>
+
+            <p>
+                <ul>
+                    <li>Überall einsetzbar</li>
+                    <li>Kein Wasser, Strom oder Chemie notwendig</li>
+                    <li>Geruchlose Nutzung, wenn Ausscheidungen getrennt werden</li>
+                    <li>Selbstständige Entsorgung der Toilettenabfälle ohne Entsorgungsstationen</li>
+                    <li>Ebenso komfortabel wie eine herkömmliche Toilette</li>
+                </ul>
+            </p>
+
+            <h5 class="mb-3 mt-5">Welche Campingtoiletten-Hersteller gibt es?</h5>
+
+            <p>
+              Mittlerweile gibt es den ein oder anderen Hersteller auf dem Markt, der sich durch besondere Qualität hervorheben konnte. Campingtoiletten von Trobolo, Trelino, Kildwick und Meine Trenntoilette sind uns hierbei besonders positiv aufgefallen. Welche Campingtoilette letztenendes für dich und deinen Camping-Trip am besten geeignet ist, kommt nicht zuletzt auch auf deine individuellen Ansprüche an. In unseren Campingtoiletten-Tests erfährst du, welche Funktionen ein bestimmtes Toilettenmodell jeweils zu bieten hat und wo dessen Stärken und Schwächen liegen. 
+            </p>
+
+            <h5 class="mb-3 mt-5">So testen wir die Campingtoiletten</h5>
+
+            <p>
+              Wir sind selbst leidenschaftliche Camper und wissen genau, worauf es bei der Auswahl der richtigen Campingtoilette ankommt. Natürlich ist uns auch bewusst, wo üblicherweise die Probleme mit solchen Toilettenlösungen stecken können.
+            </p>
+
+            <p>
+              Jede Campingtoilette, die du auf unserer Seite findest, haben wir eigenhändig getestet, um uns ein genaues Bild von ihr machen zu können. Dabei achten wir beispielsweise darauf, wie komfortabel eine Campingtoilette in der Nutzung ist und wie leicht sie sich in Wohnmobil, Van oder PKW unterbringen lässt. Außerdem ist uns wichtig, dass man die Entleerung einfach, hygienisch und eigenständig durchführen kann, ohne von Entsorgungsstationen abhängig zu sein. Natürlich achten wir auch darauf, ob eine Campingtoilette zusätzlich besonders interessante Funktionen zu bieten hat, beispielsweise geruchlose Nutzung, einen Toilettenpapierspender oder sonstige nette Details.  
+            </p>
+            
+            <p>
+                Uns geht es ganz einfach darum, dass interessierte Leser sich ein Bild machen können, welche Campingtoiletten derzeit auf dem Markt verfügbar sind, und was diese im Einzelnen zu bieten haben. Entscheiden musst du am Ende zwar selbst, aber wir möchten dich mit unserem Expertenwissen auf deiner Suche nach der perfekten Campingtoilette so gut wie möglich unterstützen, damit du entspannt deinen nächsten Campingtrip antreten kannst.  
+            </p>            
+
+            <!-- <a
+              class="btn btn-primary py-3 px-5"
+              target="_blank"
+              rel="nofollow noopener"
+              :href="product.shopLink"
+              >Bestellen</a
+            > -->
+          </div>
+          <!-- <div class="col-lg-4 fadeInUp">
+            <ProductCard :product="product" />
+            <div class="mt-5">
+              <a
+                class="btn btn-primary py-3 px-5"
+                target="_blank"
+                rel="nofollow noopener"
+                :href="product.shopLink"
+                style="display: block; width: 100%"
+                >TROBOLO Online Shop</a
+              >
+            </div>
+          </div> -->
+        </div>
+        <div class="row">
+            <div class="col-lg-2 offset-lg-5">
+              <nuxt-link
+                class="btn btn-primary py-3 px-5"
+                to="/kategorie/campingtoilette/"
+                title="Campingtoilette kaufen"
+                >Campingtoiletten kaufen
+                </nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- About End -->
+  </div>
+</template>
