d0b70fab8e8f5cb629e142a3b0081400f80d0b84
campingtoilette <campingtoilette@protonmail.com>
2022-11-18 11:17:06 +0100

    deploying

---
+++ pages/lokus-flatus.vue
@@ -0,0 +1,289 @@
+<template>
+  <div>
+    <ProductHeader
+      productName="Lokus Flatus"
+      textContent="Eleganz trifft auf Funktionalität mit der Lokus Flatus. Eine besonders leichte und kompakte Campingtoilette im Eurobox-Format. In unserem Testbericht "
+      productImage="/assets/images/campingtoiletten/campingtoilette-lokus-flatus.webp"
+    ></ProductHeader>
+
+    <!-- About Start -->
+    <div class="container-xxl py-5" id="testbericht">
+      <div class="container">
+        <div class="row g-5">
+          <div class="col-lg-8 fadeInUp" style="min-height: 400px">
+            <h2 class="mb-4">Testbericht</h2>
+            <p class="mb-4">
+              Die Lokus Flatus ist eine besonders leichte und kompakte
+              Trenntoilette, die dank Eurobox überall Platz findet. Der Clou:
+              Oben schließt die Toilette mit einem eleganten Holzdeckel ab.
+              Dieser Artikel zeigt, welche Vorteile die Toilette in puncto
+              Funktionsweise, Materialien und Komfort hat.
+            </p>
+
+            <h5 class="mb-3 mt-5">Funktionalität</h5>
+
+            <p>
+              Schon auf den ersten Blick sieht die Lokus Flatus Campingtoilette
+              durch den robusten und geölten Holzdeckel und die auffällige
+              Gravur etwas edler aus als andere Campingtoiletten. Durch den
+              strapazierfähigen Griff kann der Deckel (Birke) problemlos
+              entfernt werden, wodurch die Campingtoilette zu einem echten
+              Allrounder im Garten und Campingbereich wird.
+            </p>
+            <p>
+              Besonders hervorzuheben ist die hohe Widerstandsfähigkeit, da sich
+              die Campingtoilette nicht nur als Toilette, sondern auch als
+              Hocker und Tritt verwenden lässt. Mit einem Eigengewicht von 4,6
+              Kilogramm ist die Lokus Flatus Campingtoilette zwar ein echtes
+              Leichtgewicht, jedoch hält sie einem Gewicht von bis zu 150
+              Kilogramm stand.
+            </p>
+            <p>
+              Ebenso leicht und kompakt ist die
+              <NuxtLink
+                to="/trobolo-wandago/"
+                title="TROBOLO WandaGO Testbericht"
+                >TROBOLO WandaGo</NuxtLink
+              >
+              Campingtoilette, deren Sitzhöhe an eine normale Standardtoilette
+              angepasst ist. Diese robuste Campingtoilette besteht aus
+              hochwertigem Kunststoff, der jeder Witterung standhält, sodass die
+              Toilette bei jedem Wetter draußen stehen kann.
+            </p>
+
+            <h5 class="mb-3 mt-5">Maße</h5>
+
+            <p>
+              Die Lokus Flatus Campingtoilette kommt als Eurobox daher und
+              bringt mit einer Größe von 300 x 400 x 335 Millimetern die
+              perfekten Maße mit, um die Toilette selbst in einem Pkw zu
+              verstauen. Mit einer Sitzhöhe von 325 Millimetern ist sie weder zu
+              hoch noch zu niedrig. Wer beruflich viel unterwegs ist oder eine
+              passende Toilette für den Schrebergarten oder den Van sucht, ist
+              mit der Lokus Flatus Campingtoilette gut beraten, da sie sich dank
+              der Griffe auf allen Seiten schnell und einfach verstauen und
+              leicht handhaben lässt.
+            </p>
+
+            <h5 class="mb-3 mt-5">Material</h5>
+
+            <p></p>
+            <p>
+              Während die Lokus Flatus oben mit einem eleganten Holzdeckel
+              abschließt, besteht der Korpus aus äußerst widerstandsfähigem,
+              schwarzem Kunststoff. Das Material ist nicht nur zu 100 Prozent
+              blickdicht, sondern auch recyclingfähig. Durch die Kombination von
+              nachhaltigen Materialien ist die Lokus Flatus Campingtoilette vor
+              allem für diejenigen geeignet, die den Umweltaspekt beim Kauf
+              einbeziehen und ein umweltfreundliches und langlebiges Produkt
+              haben wollen.
+            </p>
+            <p>
+              Dank der Trennwand im Inneren der Box können die Behälter
+              zusätzlich fixiert werden, um sie vor Erschütterungen und einem
+              versehentlichen Verschütten zu schützen. Der jeweilige Füllstand
+              kann übrigens ganz einfach an der Außenseite abgelesen werden,
+              sodass die Campingtoilette nicht unbedingt geöffnet werden muss.
+            </p>
+
+            <h5 class="mb-3 mt-5">Handhabung</h5>
+
+            <p>
+              Dank des großen Fassungsvermögens müssen die Behälter nicht so
+              schnell entleert werden. Feststoffe können entweder über den
+              Hausmüll oder auf dem Kompost entsorgt werden, während flüssige
+              Ausscheidungen einfach in die Toilette gegeben werden. Der
+              Urinkanister (5 Liter mit Geruchsverschluss) und Feststoffbehälter
+              (6 Liter mit Deckel) können ebenso leicht entnommen werden wie die
+              Trennwand, die sich übrigens durch eine säurebeständige
+              PMMA-Beschichtung auszeichnet.
+            </p>
+
+            <h5 class="mb-3 mt-5">Zubehör</h5>
+
+            <p>
+              Als inkludiertes Zubehör zur Lokus Flatus Campingtoilette gehören
+              10 Liter kompostierbare Beutel sowie Premium
+              <NuxtLink
+                to="/zubehoer/einstreu/"
+                title="Campingtoilette Einstreu"
+                >Einstreu</NuxtLink
+              >
+              aus feiner Kohle, welches als Duftblocker fungiert.
+            </p>
+
+            <h5 class="mb-3 mt-5">Fazit</h5>
+
+            <p>
+              Wer eine robuste, kompakte und leichte Campingtoilette sucht, ist
+              mit der Lokus Flatus Campingtoilette gut beraten. Durch die leicht
+              zu entnehmenden Behälter kann die Toilette mit wenigen Handgriffen
+              entleert werden, wobei der Füllstand auf der Außenseite angezeigt
+              wird. Deckel und Verschlüsse garantieren eine geruchlose Nutzung,
+              daneben sorgt der elegante Holzdeckel für eine hübsche Optik.
+              Positiv ist auch, dass die Campingtoilette aufgrund der hohen
+              Widerstandsfähigkeit selbst als Hocker oder Tritt genutzt werden
+              kann.
+            </p>
+
+            <div class="row gy-2 gx-4 mb-5 mt-5">
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Funktionalität
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Materialien
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Nachhaltigkeit
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Geruchsentwicklung
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Komfort
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Entsorgung & Hygiene
+                </p>
+              </div>
+            </div>
+            <a
+              class="btn btn-primary py-3 px-5"
+              target="_blank"
+              rel="nofollow noopener"
+              href="https://www.meinetrenntoilette.de/products/lokus-flatus-trenntoilette"
+              >Bestellen</a
+            >
+          </div>
+          <div class="col-lg-4 fadeInUp">
+            <ProductCard :product="product" />
+            <div class="mt-5">
+              <a
+                class="btn btn-primary py-3 px-5"
+                target="_blank"
+                rel="nofollow noopener"
+                href="https://www.meinetrenntoilette.de/products/lokus-flatus-trenntoilette"
+                style="display: block; width: 100%"
+                >MeineTrenntoilette Online Shop</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- About End -->
+  </div>
+</template>
+
+<script>
+import products from "~/assets/products.json";
+
+const product = products.find((item) => item.name === "Lokus Flatus");
+
+export default {
+  name: "TrelinoLokusFlatus",
+  head: {
+    title: "Lokus Flatus Eurobox Campingtoilette im Test",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Testbericht zur Lokus Flatus Campingtoilette welche im Eurobox format erhältlich ist. Sie ist besonders leicht und kompakt. Jetzt Testbericht lesen.",
+      },
+      {
+        hid: "robots",
+        name: "robots",
+        content: product.seo.robots,
+      },
+    ],
+  },
+  data() {
+    return {
+      product,
+    };
+  },
+  jsonld() {
+    return {
+      "@context": "https://schema.org",
+      "@graph": [
+        {
+          "@context": "https://schema.org/",
+          "@type": "Product",
+          name: "Lokus Flatus",
+          image: [
+            "https://www.campingtoilette.shop/assets/images/campingtoiletten/campingtoilette-lokus-flatus.webp",
+          ],
+          description:
+            "Leiche und kompakte Campingtoilette im Eurobox Format. Die Trenntoilette ist besonders leicht und robust. Sie ist mit einem Holzdeckel ausgestattet.",
+          sku: "T1-04002",
+          // mpn: "24542",
+          brand: {
+            "@type": "Brand",
+            name: "Trelino",
+          },
+          review: {
+            "@type": "Review",
+            reviewRating: {
+              "@type": "Rating",
+              ratingValue: "4",
+              bestRating: "4",
+            },
+            author: {
+              "@type": "Organization",
+              name: "Campingtoilette.shop",
+            },
+          },
+          aggregateRating: {
+            "@type": "AggregateRating",
+            ratingValue: "4.3",
+            reviewCount: "79",
+          },
+          offers: {
+            "@type": "Offer",
+            url: "https://www.campingtoilette.shop/lokus-flatus/",
+            priceCurrency: "EUR",
+            price: "199.00",
+            priceValidUntil: "2024-11-20",
+            itemCondition: "https://schema.org/NewCondition",
+            availability: "https://schema.org/InStock",
+          },
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              item: {
+                "@id": "https://www.campingtoilette.shop/",
+                name: "Trockentrenntoiletten",
+              },
+            },
+            {
+              "@type": "ListItem",
+              position: 2,
+              item: {
+                "@id": "https://www.campingtoilette.shop/lokus-flatus/",
+                name: "Lokus Flatus",
+              },
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
